9156758f4be43ccc0d9d7a995f5296c7e94dc724
Ryan Rawlinson <davidrawlinson814@gmail.com>
2017-08-08 19:09:28 -0500

    Added post to toolbar.

--- app/src/main/java/com/credera/mobile/crederaall/PostEntryActivity.java
+++ app/src/main/java/com/credera/mobile/crederaall/PostEntryActivity.java
@@ -3,6 +3,9 @@ package com.credera.mobile.crederaall;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +16,8 @@ import java.util.ArrayList;
 
 public class PostEntryActivity extends AppCompatActivity {
 
+    private EditText postContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +25,21 @@ public class PostEntryActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final EditText postContent = (EditText) findViewById(R.id.post_content);
+        postContent = (EditText) findViewById(R.id.post_content);
+    }
 
-        final Button postButton = (Button) findViewById(R.id.post_button);
-        postButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        final MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_post_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_post:
                 Post post = new Post();
 
                 post.setContent(postContent.getText().toString());
@@ -37,7 +51,9 @@ public class PostEntryActivity extends AppCompatActivity {
 
                 FireBaseHelper.submitPost(ref, post);
                 finish();
-            }
-        });
+                return true;
+        }
+
+        return false;
     }
 }
