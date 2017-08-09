e0a256e0786b6904a8ad20adda6294a77f46b6ff
Michael Eamma <meamma@credera.com>
2017-08-08 19:02:38 -0500

    Add recycler view (in progress)

--- app/src/main/java/com/credera/mobile/crederaall/NewsFeedAdapter.java
+++ app/src/main/java/com/credera/mobile/crederaall/NewsFeedAdapter.java
@@ -1,8 +1,59 @@
 package com.credera.mobile.crederaall;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 /**
  * Created by meamma on 8/8/2017.
  */
 
-public class NewsFeedAdapter {
+public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public NewsFeedCard mCard;
+        public ViewHolder(NewsFeedCard v) {
+            super(v);
+            mCard = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public NewsFeedAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public NewsFeedAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        NewsFeedCard v = (NewsFeedCard) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.news_feed_card, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mCard.setText(mDataset[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
 }
--- app/src/main/java/com/credera/mobile/crederaall/NewsFeedCard.java
+++ app/src/main/java/com/credera/mobile/crederaall/NewsFeedCard.java
@@ -1,8 +1,25 @@
 package com.credera.mobile.crederaall;
 
+import android.content.Context;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 /**
  * Created by meamma on 8/8/2017.
  */
 
-public class NewsFeedCard {
+public class NewsFeedCard extends LinearLayout {
+
+    private TextView mTextView;
+
+    public NewsFeedCard(Context context) {
+        super(context);
+        inflate(context, R.layout.news_feed_card, this);
+        mTextView = (TextView) findViewById((R.id.info_text));
+    }
+
+    public void setText(String text) {
+        mTextView.setText(text);
+    }
+
 }
